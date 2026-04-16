2a0498830baf27ca2de06574790f238ef7199fea
Kanssup <misavanz@gmail.com>
2026-04-16 16:00:40 -0600

    test personaje recibe daño

--- rpg.py
+++ rpg.py
@@ -3,3 +3,14 @@ class Personaje:
         self.hp = 1000
         self.nivel = 1
         self.esta_vivo = True
+
+def test_personaje_recibe_dano():
+    heroe = Personaje()
+    enemigo = Personaje()
+    
+    # Act (Actuar)
+    enemigo.atacar(heroe, dano=200)
+    
+    # Assert
+    assert heroe.hp == 800
+
