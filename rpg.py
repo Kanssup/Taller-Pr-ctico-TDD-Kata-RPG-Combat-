75f807485a3ce74810d6b235893ff20ae4c51c0c
Kanssup <misavanz@gmail.com>
2026-04-16 16:09:38 -0600

    implemencion de ataque

--- rpg.py
+++ rpg.py
@@ -3,14 +3,6 @@ class Personaje:
         self.hp = 1000
         self.nivel = 1
         self.esta_vivo = True
-
-def test_personaje_recibe_dano():
-    heroe = Personaje()
-    enemigo = Personaje()
-    
-    # Act (Actuar)
-    enemigo.atacar(heroe, dano=200)
-    
-    # Assert
-    assert heroe.hp == 800
-
+        
+    def atacar(self, objetivo, dano):
+        objetivo.hp -= dano
