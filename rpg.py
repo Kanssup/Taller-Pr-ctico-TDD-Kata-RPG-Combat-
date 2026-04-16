6d2db88fd6c5405e36ac6dc956bae7afe0eef38a
Kanssup <misavanz@gmail.com>
2026-04-16 16:14:20 -0600

    ajustando la lógica

--- rpg.py
+++ rpg.py
@@ -3,6 +3,10 @@ class Personaje:
         self.hp = 1000
         self.nivel = 1
         self.esta_vivo = True
-        
+
     def atacar(self, objetivo, dano):
         objetivo.hp -= dano
+        if objetivo.hp <= 0:
+            objetivo.hp = 0
+            objetivo.esta_vivo = False
+
